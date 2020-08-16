0b464bc45cb91d85c0d9b77d0c096b13b096d3e4
alohajt <alohajiatao@gmail.com>
2020-08-16 12:35:27 -0600

    scroll playlist

--- src/app/share/wy-ui/wy-player/wy-player-panel/wy-player-panel.component.ts
+++ src/app/share/wy-ui/wy-player/wy-player-panel/wy-player-panel.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, Output, EventEmitter, ViewChildren, QueryList } from '@angular/core';
 import { Song } from 'src/app/services/data-types/common.types';
+import { WyScrollComponent } from '../wy-scroll/wy-scroll.component';
 
 @Component({
   selector: 'app-wy-player-panel',
@@ -7,7 +8,7 @@ import { Song } from 'src/app/services/data-types/common.types';
   styleUrls: ['./wy-player-panel.component.less']
 })
 export class WyPlayerPanelComponent implements OnInit, OnChanges {
- 
+
   @Input() songList: Song[];
   @Input() currentSong: Song;
   @Input() currentIndex: number;
@@ -15,7 +16,9 @@ export class WyPlayerPanelComponent implements OnInit, OnChanges {
 
   @Output() onClose = new EventEmitter<void>();
   @Output() onChangeSong = new EventEmitter<Song>();
-  
+
+  @ViewChildren(WyScrollComponent) private wyScroll: QueryList<WyScrollComponent>;
+
   constructor() { }
 
   ngOnInit() {
@@ -27,6 +30,14 @@ export class WyPlayerPanelComponent implements OnInit, OnChanges {
     }
     if (changes['currentSong']) {
       console.log('currentSong :', this.currentSong);
+    }
+
+
+    if (changes['show']) {
+      if (!changes['show'].firstChange && this.show) {
+        console.log('wyScroll :', this.wyScroll);
+        this.wyScroll.first.refreshScroll();
+      }
     }
   }
 }
